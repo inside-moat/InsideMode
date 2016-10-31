1f53ee652a7f4e9df61e4c33b46006ce180cc16f
junichi <insidemoat@gmail.com>
2016-10-31 23:52:31 +0000

    apply ransack

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,7 +4,8 @@ class MoviesController < ApplicationController
   # GET /movies
   # GET /movies.json
   def index
-    @movies = Movie.all
+    @q = Movie.search(params[:q])
+    @movies = @q.result(distinct: true)
   end
 
   # GET /movies/1
