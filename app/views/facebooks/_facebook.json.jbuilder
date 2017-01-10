c567f350e5184b19bb27d6b1f4d1162de10f746e
junichi <insidemoat@gmail.com>
2017-01-10 11:02:56 +0000

    facebookログイン

---
+++ app/views/facebooks/_facebook.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! facebook, :id, :created_at, :updated_at
+json.url facebook_url(facebook, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161005003812) do
+ActiveRecord::Schema.define(version: 20170106000357) do
+
+  create_table "blogs", force: :cascade do |t|
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+    t.text     "title",      limit: 65535
+    t.text     "content",    limit: 65535
+  end
 
   create_table "books", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -25,11 +32,32 @@ ActiveRecord::Schema.define(version: 20161005003812) do
     t.datetime "updated_at",               null: false
   end
 
+  create_table "facebooks", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "movies", force: :cascade do |t|
     t.text     "title",      limit: 65535
     t.datetime "runtime"
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title",        limit: 255
+    t.text     "body",         limit: 65535
+    t.date     "published_on"
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+  end
+
+  create_table "tests", force: :cascade do |t|
+    t.string   "title",        limit: 255
+    t.text     "body",         limit: 65535
+    t.date     "published_on"
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
   create_table "users", force: :cascade do |t|
