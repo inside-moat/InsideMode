477f885eda7ef404834aedf8a503c7c92bfbb605
junichi <insidemoat@gmail.com>
2017-01-12 00:54:23 +0000

    微修正

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170106000357) do
-
-  create_table "blogs", force: :cascade do |t|
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
-    t.text     "title",      limit: 65535
-    t.text     "content",    limit: 65535
-  end
 ActiveRecord::Schema.define(version: 20170109234220) do
 
   create_table "facebooks", force: :cascade do |t|
@@ -33,18 +25,6 @@ ActiveRecord::Schema.define(version: 20170109234220) do
     t.datetime "updated_at",               null: false
   end
 
-<<<<<<< HEAD
-  create_table "facebooks", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "movies", force: :cascade do |t|
-    t.text     "title",      limit: 65535
-    t.datetime "runtime"
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
-=======
   create_table "news", force: :cascade do |t|
     t.string   "title",        limit: 255
     t.text     "body",         limit: 65535
@@ -52,7 +32,6 @@ ActiveRecord::Schema.define(version: 20170109234220) do
     t.date     "published_at"
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
->>>>>>> 0c0c3a4fbb5d45b77ae3b7c6e311ee3662e41e3f
   end
 
   create_table "posts", force: :cascade do |t|
@@ -61,22 +40,6 @@ ActiveRecord::Schema.define(version: 20170109234220) do
     t.date     "published_on"
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
-<<<<<<< HEAD
-  end
-
-  create_table "tests", force: :cascade do |t|
-    t.string   "title",        limit: 255
-    t.text     "body",         limit: 65535
-    t.date     "published_on"
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-=======
->>>>>>> 0c0c3a4fbb5d45b77ae3b7c6e311ee3662e41e3f
   end
 
 end
